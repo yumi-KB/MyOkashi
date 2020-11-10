9fd23a5bcb896fcdf54bd34662521b840fe5e93b
yumi KB <bon.kyumi@gmail.com>
2020-11-10 22:33:06 +0900

    Add search bar delegate

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var searchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Delegateの通知先を設定
+        searchText.delegate = self
     }
+    
+    func searchOkashi(keyword: String) {
+    }
+}
 
 
+// MARK: UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+    // 検索ボタンがタップされた時のdelegateメソッド
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // キーボードを閉じる
+        view.endEditing(true)
+        
+        // searchBarに文字が入力されていた場合、お菓子を検索
+        if let searchWord = searchBar.text {
+            print(searchWord)
+            searchOkashi(keyword: searchWord)
+        }
+    }
+    
 }
 
